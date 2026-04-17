a651235b4b0ba186fb8fb28bedb3765e7c630443
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2026-04-17 19:48:22 +0700

    add comment

--- src/models/User.ts
+++ src/models/User.ts
@@ -72,7 +72,7 @@ export class UserModel {
     }
   }
 
-  //cari username
+  //cari semua by username
   static async findByUsername(username: string): Promise<User | null> {
     const query = 'SELECT * FROM users WHERE username = ?';
 
